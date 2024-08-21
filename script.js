8032df49607e534f193a472b8b7bbeadac4d15bb
Diego Vázquez <dvazquezlagreca@gmail.com>
2024-08-21 20:50:21 -0300

    Interfaz mas o menos completa

--- script.js
+++ script.js
@@ -15,10 +15,26 @@ class Task {
     }
 }
 
-// Cargamos informacion para testear
-let tarea1 = new Task("Colgar la ropa", "Hay que ir afuera y colgar la ropa antes que llueva", "Pepe", priorities[1], new Date ("2024-08-21"), statuses[0]);
-let tarea2 = new Task("Programar", "Picar codigo", "Pepe", priorities[1], new Date ("2024-08-21"), statuses[1]);
-let tarea3 = new Task("Ir al super", "Comprar harina, arroz, coca", "Álvaro", priorities[2], new Date ("2024-08-21"), statuses[2]);
-let tarea4 = new Task("Sacar la basura", "Dale bo", "Diego", priorities[0], new Date ("2024-08-22"), statuses[3]);
-let tarea5 = new Task("Estudiar", "Para programacion web obvio", "Juan", priorities[0], new Date ("2024-08-23"), statuses[4]);
+// crear metodo cargarDatos()
 
+// Cargamos informacion para testear
+let tarea1 = new Task("Colgar la ropa", "Hay que ir afuera y colgar la ropa antes que llueva", "Pepe", priorities[1], new Date("2024-08-21"), statuses[0]);
+let tarea2 = new Task("Programar", "Picar codigo", "Pepe", priorities[1], new Date("2024-08-21"), statuses[1]);
+let tarea3 = new Task("Ir al super", "Comprar harina, arroz, coca", "Álvaro", priorities[2], new Date("2024-08-21"), statuses[2]);
+let tarea4 = new Task("Sacar la basura", "Dale bo", "Diego", priorities[0], new Date("2024-08-22"), statuses[3]);
+let tarea5 = new Task("Estudiar", "Para programacion web obvio", "Juan", priorities[0], new Date("2024-08-23"), statuses[4]);
+
+function loadDataInHTML() {
+    const divBacklog = document.getElementById("backlog");
+    divBacklog.innerText = "<div class=\"notification is - info\" > < b > Tarea1</b > <p>Descripcion de la tarea lalala</p>  </div > ";
+
+}
+
+loadDataInHTML();
+
+/*
+                    <div class="notification is-info" >
+                        <b>Tarea1</b>
+                        <p>Descripcion de la tarea lalala</p>
+                    </div>
+*/
